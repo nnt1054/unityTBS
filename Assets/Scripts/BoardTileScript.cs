b1e9f90e314af2207fa1680416cbe9f548ac054d
Neil Toledo <ntoledo@berkeley.edu>
2019-03-09 17:36:58 -0800

    initial commit; adding moving units

---
+++ Assets/Scripts/BoardTileScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTileScript : MonoBehaviour {
+
+	public int xPos = 0;
+	public int yPos = 0;
+
+	//Get the GameObject’s mesh renderer to access the GameObject’s material and color
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D tileCollider;
+
+    Color defaultColor = new Color32(90, 155, 115, 255);
+    Color hoverColor = new Color32(80, 125, 100, 255);
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		tileCollider = GetComponent<BoxCollider2D>();
+
+		spriteRenderer.color = defaultColor;
+		Vector2 S = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
+        tileCollider.size = S;
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+
+    void OnMouseEnter()
+    {
+        spriteRenderer.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+		spriteRenderer.color = defaultColor;
+    }
+
+    void OnMouseUpAsButton() {
+    	// Debug.Log(i.ToString() + ", " + j.ToString());
+    	Debug.Log(xPos.ToString() + ", " + yPos.ToString());
+    }
+}
---
+++ Assets/Scripts/GridBoardScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBoardScript : MonoBehaviour {
+
+	public GameObject tilePrefab;
+	private int[,] gridArray = new int[,] { 
+		{ 1, 1, 1, 1 },
+		{ 1, 1, 1, 1 },
+		{ 1, 1, 1, 1 },
+	};
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < gridArray.GetLength(0); i++) {
+			int y = gridArray.GetLength(0) - i - 1;
+			for (int j = 0; j < gridArray.GetLength(1); j++) {
+				int x = j;
+				if (gridArray[i, j] == 1) {
+					var bt = Instantiate(tilePrefab, new Vector2(x, y), Quaternion.identity, this.transform);
+					var btScript = bt.GetComponent< BoardTileScript >();
+					btScript.xPos = x; btScript.yPos = y;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
